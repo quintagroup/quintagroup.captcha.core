aad7009cd1d84efb528abdd699d28d8a8bc3d532
mylan <mylan@quintagroup.com>
2010-03-18 20:31:58 +0000

    #172: Fix Quickinstaller uninstall part

--- quintagroup/captcha/core/Extensions/Install.py
+++ quintagroup/captcha/core/Extensions/Install.py
@@ -3,5 +3,5 @@ from Products.CMFCore.utils import getToolByName
 
 def uninstall(self):
     portal_setup = getToolByName(self, 'portal_setup')
-    portal_setup.runAllImportStepsFromProfile('profile-quintagroup.plonecaptchas:uninstall', purge_old=False)
+    portal_setup.runAllImportStepsFromProfile('profile-quintagroup.captcha.core:uninstall', purge_old=False)
     transaction.savepoint()
