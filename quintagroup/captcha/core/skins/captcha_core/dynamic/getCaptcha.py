26f886cb4664b6af78a085c5a621242740a39dd7
mylan <mylan@quintagroup.com>
2010-03-24 13:11:18 +0000

    #172: Fix bug in dynamic captcha: when in getCaptcha method randint return maximum number - leed to IndexError

--- quintagroup/captcha/core/skins/captcha_core/dynamic/getCaptcha.py
+++ quintagroup/captcha/core/skins/captcha_core/dynamic/getCaptcha.py
@@ -9,7 +9,9 @@
 from quintagroup.captcha.core.utils import getCaptchasCount, formKey, encrypt
 
 from random import randint
-key = formKey(randint(0, getCaptchasCount(True)))
+# *randint* return random integer in range [a, b],
+# including *both* end points.
+key = formKey(randint(0, getCaptchasCount(True)-1))
 encrypted_key = encrypt(context.captcha_key, key)
 
 return encrypted_key
