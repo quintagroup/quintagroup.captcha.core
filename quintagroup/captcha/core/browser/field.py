c150d154664411b90c6a556d7fe52813e6197896
mylan <mylan@quintagroup.com>
2010-04-01 12:22:57 +0000

    #172: Remove browser package with zope.formlib related Field and Widget - this part moved into quintagroup.formlib.captcha package

--- quintagroup/captcha/core/browser/field.py
+++
@@ -1,6 +0,0 @@
-from zope.interface import implements
-from zope.schema import ASCIILine
-from quintagroup.captcha.core.browser.interfaces import ICaptcha
-
-class Captcha(ASCIILine):
-    implements(ICaptcha)
